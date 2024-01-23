578e00c39f47a301929b517586c40bca94ecb38a
apps <dalma.admins@nyu.edu>
2024-01-23 17:13:56 +0400

    Minor chnage to v100_submit

--- Gpu_Benchmarking/v100_submit_tensorflow.sh
+++ Gpu_Benchmarking/v100_submit_tensorflow.sh
@@ -14,13 +14,14 @@
 
 #Setting up job name
 #SBATCH -J jobname
-#SBATCH -o Results_a100/output_%j.log
+#SBATCH -o Results_v100/output_%j.log
 
 #Resource requiremenmt commands end here
 
 #activate conda environment
 source /share/apps/NYUAD5/miniconda/3-4.11.0/bin/activate
 conda activate tf_gpu_new
+#conda activate new_tensorflow_2.11
 
 #Execute the code
 cd /scratch/apps/Benchmarking/Gpu_Benchmarking
@@ -28,4 +29,4 @@ python bench_tensorflow.py
 
 #Change output file name to include date
 time=`date +%Y%m%d-%H%M%S`
-mv Results_a100/output_$SLURM_JOB_ID.log Results_a100/${time}_output.log
+mv Results_v100/output_$SLURM_JOB_ID.log Results_v100/${time}_output.log
